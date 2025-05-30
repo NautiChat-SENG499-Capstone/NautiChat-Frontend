1a51052da4560f619fac608fa660685fa0152340
shalashaban <shalashaban@uvic.ca>
2025-05-29 19:01:27 -0700

    testing

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Hello2</h1>
+      <h1>Hello2. Testing LAKAKAAA</h1>
     </main>
   );
 }
