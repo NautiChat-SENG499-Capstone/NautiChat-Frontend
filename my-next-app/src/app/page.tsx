c70fbb10bda909b6a9a3cd968c5c3b3b09d60cc3
Tyler Le <tyler0308gm@gmail.com>
2025-05-29 18:32:11 -0700

    changed to hellp Shaban

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Hello</h1>
-      <p>Hello</p>
+      <p>Hello Shabannnn</p>
     </main>
   );
 }
