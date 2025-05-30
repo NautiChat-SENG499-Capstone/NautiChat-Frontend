9c5a8da67f798f5408641be3b3f6b45f0630456f
Tyler Le <tyler0308gm@gmail.com>
2025-05-29 18:55:47 -0700

    Update Page2

--- my-next-app/src/app/page2.tsx
+++ my-next-app/src/app/page2.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>LAKAKA</h1>
+      <h1>LUKAKU LIKES YOU</h1>
       <p></p>
     </main>
   );
