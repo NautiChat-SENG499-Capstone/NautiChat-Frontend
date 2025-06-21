5b845425323cb456c60e1c0f91da396cb59f05c1
Joel C <joel.chamberlain38@gmail.com>
2025-06-21 14:12:31 -0700

    Fixing chat area spacing

--- my-next-app/src/components/ChatArea.tsx
+++ my-next-app/src/components/ChatArea.tsx
@@ -50,7 +50,7 @@ export function ChatArea({
     <div className="flex-1 flex flex-col min-h-0">
       <ScrollArea ref={scrollAreaRef} className="flex-1 min-h-0">
         <div className="p-4">
-          <div className="max-w-4xl mx-auto space-y-4">
+          <div className="max-w-1xl mx-auto space-y-4">
             {messages.map((message) => (
               <div key={message.id}>
                 <div className={`flex ${message.role === "user" ? "justify-end" : "justify-start"}`}>
